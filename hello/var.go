b75e34ee8d2254a0ba3b93982ba1619050a2c92f
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-06-16 13:24:43 -0300

    variaveis

---
+++ hello/var.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Sérgio"
+	var idade int = 21
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr. ", nome, ", sua idade é ", idade)
+	fmt.Println("Este programa esta na versao: ", versao)
+}
